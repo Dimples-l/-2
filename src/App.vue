a57be007636457fbd2a56b62b2426b845dca135b
Yoki <1426054790@qq.com>
2020-05-19 19:29:37 +0800

    1
    home

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
+  <div class="nav">
+    <div class="web-title">hy</div>
+    <div class="nav1">
+      <span><router-link to="/">首页</router-link></span>
+      <span><router-link to="">登录</router-link></span>
+      <span><router-link to="">注册</router-link></span>
+      <span><router-link to="">收藏</router-link></span>
+      <span><router-link to="">我的</router-link></span>
     </div>
-    <router-view/>
+    <div class="search">
+      <input type="text" name="search" id="search">
+      <button>搜索</button>
+    </div>
   </div>
+
+  <!-- <router-view /> -->
 </template>
 
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  a{
+    color: grey;
+    text-decoration: none;
+  }
+  .nav{
+    position: relative;
+    background-color: black;
+    opacity: 0.7;
+    height: 82px;
+  }
+  .web-title{
+    position: absolute;
+    font-size:50px;
+    color: white;
+    margin-left: 10%;
+    float: left;
+    top:10px;
+  }
+  .nav1{
+    /* position: relative; */
+    text-align: right;
+    width: 600px;
+    float: left;
+    margin-top: 30px;
+    left: 100px;
+  }
+  span{
+    margin-left: 50px;
+  }
+  .search{
+    margin-top: 30px;
+    margin-left: 30px;
+    float: left;
+  }
+  #search{
+    border-radius: 10px;
+    width: 300px;
+    height: 20px;
+  }
+  button{
+    border: none;
+    border-radius: 5px;
+    margin-left: 10px;
+  }
 </style>
+
+<script>
+// import content from './components/content'
+export default {
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
---
+++ src/components/content.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="user">username</header>
+    <div class="container">
+        <div class="img">
+            <img src="../assets/logo.png" alt="">
+        </div>
+        <div class="text">1111111111111</div>
+    </div>
+    <footer>
+        <span>点赞</span>
+        <span>评论</span>
+        <span>收藏</span>
+        <span>分享</span>
+    </footer>
+</template>
+
+<style lang="stylus">
+    .img .text{
+        float left;
+    }
+</style>
