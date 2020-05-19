8f3d50532e86c8c05f1c12b9c9ec4c3841b5a09a
Yoki <1426054790@qq.com>
2020-05-19 16:26:02 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="nav">
+    <div class="web-title">hy</div>
+    <div class="nav1">
+      <ul><router-link to="/">首页</router-link></ul>
+      <ul><router-link to="">登录</router-link></ul>
+      <ul><router-link to="">注册</router-link></ul>
+      <ul><router-link to="">收藏</router-link></ul>
+      <ul><router-link to="">我的</router-link></ul>
+    </div>
+    <div class="search">
+      <input type="text" name="search" id="search">
+      <button>搜索</button>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+  a{
+    color: grey;
+    text-decoration: none;
+  }
+  .nav{
+    position: relative;
+    background-color: black;
+    opacity: 0.7;
+    height: 82px;
+  }
+  .web-title{
+    position: absolute;
+    font-size:50px;
+    color: white;
+    margin-left: 10%;
+  }
+  .nav1{
+    position: relative;
+    text-align: right;
+    width: 600px;
+  }
+  ul{
+    display: inline;
+  }
+  .search{
+    margin-top: 30px;
+    margin-left: 30px;
+  }
+  #search{
+    border-radius: 10px;
+  }
+  button{
+    border: none;
+    border-radius: 8px;
+    /* background-color: cornflowerblue; */
+    margin-left: 10px;
+  }
+</style>
 
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {}
   }
 }
+
 </script>
